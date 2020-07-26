fa5d9cf16332700f9a969d6afb7af9473d6873ba
Bryan Lincoln <bryanufg@gmail.com>
2020-07-26 20:38:09 -0300

    Update coinrun to procgen env

--- rlkit/envs/procgen.py
+++ rlkit/envs/procgen.py
@@ -5,9 +5,18 @@ import torch
 from . import register_env
 
 
-@register_env("coinrun")
-class CoinRunEnv:
-    def __init__(self, n_tasks=60, n_train_tasks=50, n_test_tasks=10, seed=0, **kwargs):
+@register_env("procgen")
+class ProcgenEnv:
+    def __init__(
+        self,
+        n_tasks=60,
+        n_train_tasks=50,
+        n_test_tasks=10,
+        seed=0,
+        distribution_mode="easy",
+        env_names="procgen:procgen-coinrun-v0",
+        **kwargs
+    ):
         self._seed = seed
         self.n_tasks = n_tasks
         self.n_train_tasks = n_train_tasks
@@ -15,11 +24,13 @@ class CoinRunEnv:
         self.tasks = []
         self._sample_tasks()  # fill up self.tasks
         self._goal = 0
+        self.distribution_mode = distribution_mode
+        self.env_names = env_names
         self.env = self.tasks[0]  # this env will change depending on the idx
 
         self.observation_space = self.env.observation_space
 
-        # coinrun action space is Discrete(15)
+        # action space is Discrete(15)
         # we need to transform it to continuous
         # so we use an one-hot approach
         self.action_space = spaces.Box(low=0, high=1, shape=(15,))
@@ -51,6 +62,6 @@ class CoinRunEnv:
 
     def _sample_tasks(self):
         self.tasks = [
-            gym.make("procgen:procgen-coinrun-v0", start_level=idx, num_levels=1)
+            gym.make(self.env_names, start_level=idx, num_levels=1, distribution_mode=self.distribution_mode)
             for idx in range(self._seed, self._seed + self.n_tasks)
         ]
